f019dd77f5451befba4c5126baf987a3c46b9657
Usama Muhammad <usamamuhammad45678@gmail.com>
2021-05-21 14:51:25 +0500

    changed header wrapper, looks better

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App() {
       </div>
 
       <HeaderCard/>
+     
       </div>
       
       {/* tabs body */}
--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -18,55 +18,41 @@ function Body() {
         if((id=='visual')){
             
             dispatch(toggleVisual(!selectedTabVisual))
-            console.log("aaaaaaaaa"+selectedTabVisualL)
-
-        //    setSelectedTabVisual((prev)=>{ return !prev})
            
             if(true){
                 dispatch(toggleAuditory(false))
                 dispatch(toggleKinesthetic(false))
                 dispatch(toggleReflective(false))
-                // setSelectedTabAuditory(false)
-                // setSelectedTabKinesthetic(false)
-                // setSelectedTabReflective(false)
             }
            
             
         }
         else if((id=='auditory')){
              dispatch(toggleAuditory(!selectedTabAuditory))
-            // setSelectedTabAuditory((prev)=>!prev)
+           
             if(true){
                 dispatch(toggleVisual(false))
                 dispatch(toggleKinesthetic(false))
                 dispatch(toggleReflective(false))
-                // setSelectedTabVisual(false)
-                // setSelectedTabKinesthetic(false)
-                // setSelectedTabReflective(false)
             }
         }
         else if( (id=='kinesthetic')){
             dispatch(toggleKinesthetic(!selectedTabKinesthetic))
-            // setSelectedTabKinesthetic((prev)=>!prev)
+           
             if(true){
                 dispatch(toggleVisual(false))
                 dispatch(toggleAuditory(false))
                 dispatch(toggleReflective(false))
-                // setSelectedTabVisual(false)
-                // setSelectedTabAuditory(false)
-                // setSelectedTabReflective(false)
             }
         }
         else if((id=='reflective')){
             dispatch(toggleReflective(!selectedTabReflective))
-            // setSelectedTabReflective((prev)=>!prev)
+
             if(true){
                 dispatch(toggleVisual(false))
                 dispatch(toggleAuditory(false))
                 dispatch(toggleKinesthetic(false))
-                // setSelectedTabVisual(false)
-                // setSelectedTabAuditory(false)
-                // setSelectedTabKinesthetic(false)
+                
             }
         }
     }
@@ -98,9 +84,7 @@ function Body() {
              <SubjectSection subject="History And Geography" />
              <SubjectSection subject="Primary School Resources" />
              <SubjectSection subject="Secondary School Resources" />
-{/* 
-<SubjectSection subject="English" title="English Speaking by the webapp part one" description=" Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut purus ex. Praesent in velit eget nulla pharetra malesuada quis vitae nulla. Mauris faucibus, lorem at tempor efficitur, enim dolor tincidunt eros, id mollis nunc arcu eu mi. a malesuada quis vitae nulla. Maur"
-              tutorName="Arsad Ansary" thumbnail={header1} link="https://www.google.com/" /> */}
+
         </div>
     )
 }
